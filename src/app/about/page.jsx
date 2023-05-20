cb72e08351ec6938db49a4bcc29c6bd9a9c8b1b5
Amanuel Galema <amexabee@yahoo.com>
2023-05-20 10:10:18 +0300

    Remove boilerplate code, add custom components

---
+++ src/app/about/page.jsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+        repellendus a nihil odio incidunt quo rerum sequi eveniet, aut ea
+        possimus dolor maiores beatae voluptas, provident illo nemo? Ut,
+        assumenda?
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fuga
+        repellendus inventore illum, odit molestiae quibusdam amet debitis
+        exercitationem quisquam! Non itaque, omnis odio ducimus minus ipsam ab
+        adipisci esse.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
