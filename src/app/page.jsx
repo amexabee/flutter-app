e4a26803ddacdca862a367c8a3b74de44b2475e8
Amanuel Galema <amexabee@yahoo.com>
2023-05-21 18:14:56 +0300

    Complete logic to fetch and display courses

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Courses from './components/courses';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome</h1>
+      <Courses />
     </div>
   );
 };
