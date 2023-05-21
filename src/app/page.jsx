81c8966526986a34c77a5af1c5748a9bf9e2202f
Amanuel Galema <amexabee@yahoo.com>
2023-05-21 18:44:30 +0300

    Implement searching

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,32 @@
+'use client';
 import Link from 'next/link';
 import Courses from './components/courses';
+import { useEffect, useState } from 'react';
+import LoadingPage from './loading';
+import CouseSearch from './components/courseSearch';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const response = await fetch('/api/courses');
+      const courses = await response.json();
+      setCourses(courses);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) return <LoadingPage />;
+
   return (
     <div>
       <h1>Welcome</h1>
-      <Courses />
+      <CouseSearch getSearchResults={(courses) => setCourses(courses)} />
+      <Courses courses={courses} />
     </div>
   );
 };
